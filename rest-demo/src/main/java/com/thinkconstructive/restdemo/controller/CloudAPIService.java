e60420e2f389c73df62129ac06cb5c2daf019546
Somossy László <stony86@gmail.com>
2023-10-01 11:50:35 +0200

    Initial commit

---
+++ rest-demo/src/main/java/com/thinkconstructive/restdemo/controller/CloudAPIService.java
@@ -0,0 +1,32 @@
+package com.thinkconstructive.restdemo.controller;
+import com.thinkconstructive.restdemo.model.CloudVendor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/cloudvendor")
+public class CloudAPIService {
+
+    CloudVendor cloudVendor;
+    @GetMapping("{vendorId}")
+    public CloudVendor getCloudVendorDetails(String vendorId) {
+        return cloudVendor;
+    }
+
+    @PostMapping
+    public String createCloudVendorDetails(@RequestBody CloudVendor cloudVendor) {
+        this.cloudVendor = cloudVendor;
+        return "Cloud Vendor Created Successfully";
+    }
+
+    @PutMapping
+    public String updateCloudVendorDetails(@RequestBody CloudVendor cloudVendor) {
+        this.cloudVendor = cloudVendor;
+        return "Cloud Vendor Updated Successfully";
+    }
+
+    @DeleteMapping("{vendorId}")
+    public String deleteCloudVendorDetails(String vendorId) {
+        this.cloudVendor = null;
+        return "Cloud Vendor Deleted Successfully";
+    }
+}
